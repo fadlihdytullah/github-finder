52a580682f0067718e591499453cad59ac9a2b03
Fadli Hidayatullah <fadlihdytullah.dev@gmail.com>
2020-08-14 10:22:21 +0700

    Refactor Search

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,78 +1,75 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Alert from "../layouts/Alert";
 
-class Search extends Component {
-  state = {
-    searchTerm: "",
-    showAlert: false,
-  };
+const Search = ({ showClear, onSearch, onClear }) => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [showAlert, setShowAlert] = React.useState(false);
 
-  static propTypes = {
-    showClear: PropTypes.bool.isRequired,
-    onSearch: PropTypes.func.isRequired,
-    onClear: PropTypes.func.isRequired,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.showAlert) {
+  React.useEffect(() => {
+    if (showAlert) {
       setTimeout(() => {
-        this.setState({ showAlert: false });
+        setShowAlert(false);
       }, 3000);
     }
-  }
+  }, [showAlert]);
 
-  handleChange = (event) => this.setState({ searchTerm: event.target.value });
+  const handleChange = (event) => setSearchTerm(event.target.value);
 
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    if (!this.state.searchTerm) {
-      this.setState({ showAlert: true });
+    if (!searchTerm) {
+      setShowAlert(true);
       return;
     }
 
-    this.props.onSearch(this.state.searchTerm);
+    onSearch(searchTerm);
   };
 
-  handleClear = () => {
-    this.setState({ searchTerm: "", showAlert: false });
-    this.props.onClear();
+  const handleClear = () => {
+    setSearchTerm("");
+    setShowAlert(false);
+    onClear();
   };
 
-  render() {
-    return (
-      <div>
-        {this.state.showAlert && (
-          <Alert
-            message='Please input the
+  return (
+    <div>
+      {showAlert && (
+        <Alert
+          message='Please input the
           search field'
-          />
-        )}
+        />
+      )}
 
-        <form className='form' onSubmit={this.handleSubmit}>
-          <input
-            type='text'
-            placeholder='Search users...'
-            value={this.state.searchTerm}
-            onChange={this.handleChange}
-          />
-          <button type='submit' className='btn btn-dark btn-block'>
-            Search
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Search users...'
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <button type='submit' className='btn btn-dark btn-block'>
+          Search
+        </button>
+
+        {showClear && (
+          <button
+            type='button'
+            className='btn btn-block my-1'
+            onClick={handleClear}
+          >
+            Clear
           </button>
+        )}
+      </form>
+    </div>
+  );
+};
 
-          {this.props.showClear && (
-            <button
-              type='button'
-              className='btn btn-block my-1'
-              onClick={this.handleClear}
-            >
-              Clear
-            </button>
-          )}
-        </form>
-      </div>
-    );
-  }
-}
+Search.propTypes = {
+  showClear: PropTypes.bool.isRequired,
+  onSearch: PropTypes.func.isRequired,
+  onClear: PropTypes.func.isRequired,
+};
 
 export default Search;
